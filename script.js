8077574664cffcb28b21aae2ab3df57b841a9191
textencoder <160430049+textencoder@users.noreply.github.com>
2025-01-26 13:27:43 -0600

    functional dropdown menu

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('button');
+const menu = document.getElementById('options');
+const icon = document.querySelector('svg');
+
+button.addEventListener('click', () => {
+    menu.classList.toggle('invisible');
+    menu.classList.toggle('visible');
+    button.classList.toggle('active');
+
+    icon.innerHTML = menu.classList.contains('invisible') ? 
+    `<path
+          d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4M7,10L12,15L17,10H7Z"
+        />`
+    : `<path d="M12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20M17,14L12,9L7,14H17Z" />`;
+})
